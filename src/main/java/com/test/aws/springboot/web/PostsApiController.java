b522062c2b21761e0c92404f49f1ffcde14d6ac1
dudeoeoeo <dudekekek@naver.com>
2021-09-29 21:26:47 +0900

    posts update, save, apiController

--- src/main/java/com/test/aws/springboot/web/PostsApiController.java
+++ src/main/java/com/test/aws/springboot/web/PostsApiController.java
@@ -17,6 +17,7 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        System.out.println("Post /api/v1/posts called: "+requestDto.toString());
         return postsService.save(requestDto);
     }
 
@@ -27,5 +28,12 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        System.out.println("Delete called id = "+id);
+        postsService.delete(id);
+        return id;
     }
 }
