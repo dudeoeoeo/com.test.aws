5fba30cd1b13cd59ae489e9a406e22ac2a2cdef9
dudeoeoeo <dudekekek@naver.com>
2021-10-03 13:30:06 +0900

    Naver add

--- src/main/java/com/test/aws/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/test/aws/springboot/config/auth/dto/OAuthAttributes.java
@@ -18,6 +18,7 @@ public class OAuthAttributes {
 
     @Builder
     public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        System.out.println("OAuthAttributes 호출");
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -27,10 +28,32 @@ public class OAuthAttributes {
 
     // OAuth2User 에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야 한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        System.out.println("OAuthAttributes of 호출");
+        if("naver".equals(registrationId))
+            return ofNaver("id", attributes);
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        System.out.println("OAuthAttributes ofNaver 호출");
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        System.out.println("response: "+response.toString());
+        System.out.println("name: "+(String) response.get("name"));
+        System.out.println("email: "+(String) response.get("email"));
+        System.out.println("profile_image: "+(String) response.get("profile_image"));
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        System.out.println("OAuthAttributes ofGoogle 호출");
+        System.out.println("ofGoogle attributes: "+attributes.toString());
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -43,6 +66,8 @@ public class OAuthAttributes {
     // User Entity를 생성
     // 처음 가입할 때 기본 권한을 GUEST로 준다
     public User toEntity() {
+        System.out.println("User toEntity 호출");
+        System.out.println("name: "+name+", email: "+email+", picture: "+picture);
         return User.builder()
                 .name(name)
                 .email(email)
