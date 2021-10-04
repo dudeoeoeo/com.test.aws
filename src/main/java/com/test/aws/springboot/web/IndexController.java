41f0c067809b4fe9bde89a2058edf1cd9f23d329
dudeoeoeo <dudekekek@naver.com>
2021-10-04 16:34:10 +0900

    IndexController

--- src/main/java/com/test/aws/springboot/web/IndexController.java
+++ src/main/java/com/test/aws/springboot/web/IndexController.java
@@ -1,24 +1,39 @@
 package com.test.aws.springboot.web;
 
+import com.test.aws.springboot.config.auth.LoginUser;
+import com.test.aws.springboot.config.auth.dto.SessionUser;
+import com.test.aws.springboot.domain.user.Role;
+import com.test.aws.springboot.domain.user.User;
 import com.test.aws.springboot.service.posts.PostsService;
 import com.test.aws.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     // mustache 스타터가 컨트롤러에서 문자열을 반환할 때 경로와 확장자 지정
     // 기본 경로 src/main/resources/templates
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        System.out.println("index called: "+user);
+        if(user != null) {
+            System.out.println("user: "+user.getEmail()+", "+user.getName());
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
