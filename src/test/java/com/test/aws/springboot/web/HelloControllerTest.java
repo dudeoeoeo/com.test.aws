b74b89a6aac9a180eb74f4070733ec4d95bda1bd
dudeoeoeo <dudekekek@naver.com>
2021-10-08 08:28:58 +0900

    HelloControllerTest 수정

--- src/test/java/com/test/aws/springboot/web/HelloControllerTest.java
+++ src/test/java/com/test/aws/springboot/web/HelloControllerTest.java
@@ -1,15 +1,21 @@
 package com.test.aws.springboot.web;
 
+import com.test.aws.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행
 // 여기서는 SpringRunner 라는 스프링 실행자 사용
@@ -19,7 +25,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션
 // 선언할 경우 @Controller, @ControllerAdvice 사용 가능
 // @Service, @Component, @Repository 사용 불가
-@WebMvcTest(controllers = HelloController.class)
+// WebMvcTest는 @Service, @Component, @Repository 이 어노테이션들이 스캔 대상이 아니다.
+// @WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     // 웹 API 테스트 시 사용
@@ -28,6 +41,7 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello() throws Exception {
         String hello = "hello";
@@ -44,6 +58,7 @@ public class HelloControllerTest {
                 // Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
                 .andExpect(content().string(hello));
     }
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto() throws Exception {
         String name = "hello";
